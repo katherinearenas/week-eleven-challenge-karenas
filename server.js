a5e6e1ab06563d0de9b59a8bb8058f6be7310cf8
Katherine <89894177+katherinearenas@users.noreply.github.com>
2024-08-15 13:11:50 -0500

    got first get working

--- server.js
+++ server.js
@@ -1,36 +1,59 @@
+// const express = require('express');
+// const path = require('path');
+// const { clog } = require('./middleware/clog');
+// const api = require('./routes/index.js');
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// // Import custom middleware, "cLog"
+// app.use(clog);
+
+// // Middleware for parsing JSON and urlencoded form data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api)
+
+// app.use(express.static('public'));
+
+// // GET Route for homepage
+// // app.get('/', (req, res) =>
+// //   res.sendFile(path.join(__dirname, './public/index.html'))
+// // );
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+
+// // // GET Route for feedback page
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, './public/notes.html'))
+// );
+
+// // Wildcard route to direct users to a 404 page
+// // app.get('*', (req, res) =>
+// //   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// // );
+
+// app.listen(PORT, () =>
+//   console.log(`App listening at http://localhost:${PORT}`)
+// );
+
 const express = require('express');
 const path = require('path');
-const htmlRoutes = require('./routes/index.js');
-
-const PORT = process.env.PORT || 3001;
+const noteData = require('./db/db.json');
+const PORT = 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(clog);
-app.use('./index', htmlRoutes);
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
 app.use(express.static('public'));
 
-// GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/index.html'))
-// );
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+app.get('/api/notes', (req, res) => res.json(noteData));
 
-// Wildcard route to direct users to a 404 page
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-// );
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
